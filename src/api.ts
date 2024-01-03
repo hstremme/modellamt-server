0506754da22249cb041e83297ee5ef7967175aa6
Stremme <heinrich.g.stremme@t-online.de>
2024-01-03 23:40:42 +0100

    updated ressource/post, added ressources/delete and gamestate/get

--- src/api.ts
+++ src/api.ts
@@ -28,49 +28,70 @@ router.post('/ressource', async (req: express.Request, res: express.Response) =>
         return;
     }
     try {
-        const response = await GameState.updateOne({ _id: req.query.id },{ 
+        const filter = { '_id': req.query.gameId as string};
+        await GameState.updateOne( filter,{ 
             budget: newBudget,
             time: newTime,
             $set: {
                 [`ressources.${ressource.name}`]: gameState.ressources.get(ressource.name)! + 1
             }
         });
-        console.log(response);
     } catch (e: any) {
         res.status(500).send('Updating GameState failed: ' + e);
         return;
     }
-    res.send(gameState);
+    res.status(200).send({newBudget});
 });
 
-// TODO Delete
-router.post('/add/ressource', async (req: express.Request, res: express.Response) => {
-    const randomRes = new Ressource({ 
-        name: 'Mitarbeiter', 
-        info: 'Lange Information ueber die Ressource', 
-        isExpandable: true, 
-        price: 180, 
-        baseTime: 550 
-    });
-    randomRes.save()
-    .catch((e: any) => res.status(500).send(e))
-    .then(() => res.status(200).send('Ressource added succesfully'))
+router.delete('/ressource', async (req: express.Request, res: express.Response) => {
+    if (!req.query.resId || !req.query.gameId ) {
+        res.status(400).send('Parameters missing.');
+        return;
+    }
+    const ressource = await Ressource.findById(req.query.resId);
+    const gameState = await GameState.findById(req.query.gameId);
+    if (ressource == null || gameState == null){
+        res.status(400).send('DB entry not found.');
+        return;
+    }
+    // Checks if the Ressource can be expanded
+    if (gameState.ressources.get(ressource.name)! < 1){
+       res.status(500).send('Ressource not existing.');
+       return;
+    }
+    const newBudget = gameState.budget + ressource.price;
+    const newTime = gameState.time + ressource.baseTime;
+    try {
+        const filter = { '_id': req.query.gameId as string};
+        await GameState.updateOne( filter,{ 
+            budget: newBudget,
+            time: newTime,
+            $set: {
+                [`ressources.${ressource.name}`]: gameState.ressources.get(ressource.name)! - 1
+            }
+        });
+    } catch (e: any) {
+        res.status(500).send('Updating GameState failed: ' + e);
+        return;
+    }
+    res.status(200).send({newBudget});
 });
 
-// TODO Delete
-router.post('/add/gamestate', async (req: express.Request, res: express.Response) => {
-    const randomState = new GameState({ 
-        budget: 5000, 
-        time: 1000, 
-        ressources: { 
-            'Mitarbeiter': 2,
-            'ScanStrecke': 1,
-        },
-        scenario: 2, 
-    });
-    randomState.save()
-    .catch((e: any) => res.status(500).send(e))
-    .then(() => res.status(200).send('State added succesfully'))
+router.get('/gamestate', async (req: express.Request, res: express.Response) => {
+    if (!req.query.gameId){
+        res.status(400).send('Params missing.');
+        return;
+    }
+    try {
+        const gameState = await GameState.findById(req.query.gameId);
+        if (gameState == null){
+            res.status(400).send('Entry not found');
+            return;
+        }
+        res.status(200).send(gameState);
+    } catch (e) {
+        res.status(500).send('DB Query failed: ' + e);
+    }
 });
 
 export { router as api };
